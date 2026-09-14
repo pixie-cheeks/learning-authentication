4b7548a393c67e52a04cc1f32be3d6010bbdb7ee
pixie-cheeks <121865202+pixie-cheeks@users.noreply.github.com>
2026-09-14 18:29:12 +0530

    Add main script file

---
+++ app.js
@@ -0,0 +1,27 @@
+import { join } from 'node:path';
+import { Pool } from 'pg';
+import express, { urlencoded } from 'express';
+import session from 'express-session';
+import { session as _session } from 'passport';
+import { Strategy as LocalStrategy } from 'passport-local';
+
+const pool = new Pool({
+  // add your configuration
+});
+
+const app = express();
+app.set('views', join(import.meta.dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(session({ secret: 'cats', resave: false, saveUninitialized: false }));
+app.use(_session());
+app.use(urlencoded({ extended: false }));
+
+app.get('/', (_req, res) => res.render('index'));
+
+app.listen(3000, (error) => {
+  if (error) {
+    throw error;
+  }
+  console.log('app listening on port 3000!');
+});
