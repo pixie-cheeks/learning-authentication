b3ef4daaf76f8ac1d24f04f17ff12fd9f55b5575
pixie-cheeks <121865202+pixie-cheeks@users.noreply.github.com>
2026-09-14 19:26:07 +0530

    Add strategy serialization functions

--- app.js
+++ app.js
@@ -40,6 +40,23 @@ passport.use(
   }),
 );
 
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const { rows } = await pool.query('SELECT * FROM users WHERE id = $1', [
+      id,
+    ]);
+    const user = rows[0];
+
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
 app.post('/sign-up', async (req, res) => {
   await pool.query('INSERT INTO users (username, password) VALUES ($1, $2)', [
     req.body.username,
