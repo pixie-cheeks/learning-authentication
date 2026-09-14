338558741f8324cbc2711963d359cc4b6bca67aa
pixie-cheeks <121865202+pixie-cheeks@users.noreply.github.com>
2026-09-14 20:10:15 +0530

    Use bcrypt to hash and compare passwords

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import { join } from 'node:path';
 import { Pool } from 'pg';
+import bcrypt from 'bcryptjs';
 import express, { urlencoded } from 'express';
 import session from 'express-session';
 import passport from 'passport';
@@ -30,7 +31,8 @@ passport.use(
       if (!user) {
         return done(null, false, { message: 'Incorrect username' });
       }
-      if (user.password !== password) {
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
         return done(null, false, { message: 'Incorrect password' });
       }
       return done(null, user);
@@ -71,9 +73,10 @@ app.post(
   }),
 );
 app.post('/sign-up', async (req, res) => {
+  const hashedPassword = await bcrypt.hash(req.body.password, 10);
   await pool.query('INSERT INTO users (username, password) VALUES ($1, $2)', [
     req.body.username,
-    req.body.password,
+    hashedPassword,
   ]);
   res.redirect('/');
 });
